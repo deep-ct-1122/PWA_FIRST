88e3c1205befcbd3ff23d20f1f26f584256318e7
deep-ct-1122 <160468709+deep-ct-1122@users.noreply.github.com>
2024-12-10 17:56:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js', {scope: '.'})
+    navigator.serviceWorker.register('sw.js')
         .then(function () {
             console.log('ServiceWorker Registered')
         })
