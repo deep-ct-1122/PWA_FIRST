b210b9842ae1744e4868299ace3a6cf9650dcf3d
deep-ct-1122 <160468709+deep-ct-1122@users.noreply.github.com>
2024-12-10 16:50:05 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js')
+        .then(function () {
+            console.log('ServiceWorker Registered')
+        })
+        .catch(function (error) {
+            console.log('SW Regitration Failed:', error)
+        })
+} else {
+    console.log('SW not present in navigator')
+}
---
+++ constants.js
@@ -0,0 +1,1 @@
+const landingTitle = 'Start Earning Rewards Today'
